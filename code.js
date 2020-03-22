9bac440c6e73cea0607436afd5c6e9dedfcd9615
Phu Pham <phupham07@yahoo.com>
2020-03-21 21:20:34 -0700

    Need to fix nested for loops

--- code.js
+++ code.js
@@ -1,6 +1,11 @@
 var container = document.querySelector('.container');
 
-var div = 16
-for(var i = 0; i < div; i++) {
-    container.innerHTML += '<div class ="box"></div>';
+var div = document.createElement('div'); //i could use another variable to change this later
+div.classList.add('.box');
+for(var i = 0; i < 16; ++i) {
+    for( var j = 0; j < 16; ++i) {
+        // container.innerHTML += '<div class ="box"></div>';
+        container.appendChild(div);
+    }
+    var br = document.createElement('break');
 }
