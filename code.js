19cf083f9f60b68f7d6d5047eab29c845747673d
Phu Pham <phupham07@yahoo.com>
2020-03-23 14:37:23 -0700

    change reset button to remove the colors instead of refresh the page

--- code.js
+++ code.js
@@ -1,14 +1,20 @@
-let container = document.querySelector('.container');
+let boxes = document.querySelector('.boxes');
 for(var y = 0; y < 16; ++y) {
     for( var x = 0; x < 16; ++x) {
-        container.innerHTML += '<div class ="box"></div>';
+        boxes.innerHTML += '<div class ="box"></div>';
         if(x == 16) break;
     }
 }
-
 let hover = document.getElementsByClassName('box');
 for(var i = 0; i < hover.length; i++) {
     hover[i].addEventListener('mouseover', (event) => {
         event.target.style.background = 'black';
     });
 }
+
+
+let container = document.querySelector('.container');
+let button = document.createElement('button');
+button.textContent = 'Reset';
+button.addEventListener('click', () => window.location.reload());
+container.appendChild(button);
