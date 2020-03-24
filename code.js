13af876373bcb7b5a23ba7defd6f3352fc1b510e
Phu Pham <phupham07@yahoo.com>
2020-03-23 21:02:28 -0700

    need to fix reset button to stay still and stand on top of boxes

--- code.js
+++ code.js
@@ -1,3 +1,4 @@
+//These are the codes to make small boxes
 let boxes = document.querySelector('.boxes');
 for(var y = 0; y < 16; ++y) {
     for( var x = 0; x < 16; ++x) {
@@ -12,9 +13,12 @@ for(var i = 0; i < hover.length; i++) {
     });
 }
 
-
+//These are the codes to make a reset button
+//1) need to find a way to reset small boxes' color
+//2) need to find a way to make the reset button on top
 let container = document.querySelector('.container');
 let button = document.createElement('button');
+button.classList.add('reset');
 button.textContent = 'Reset';
 button.addEventListener('click', () => window.location.reload());
 container.appendChild(button);
