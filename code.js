a92c85f08243715828ed8777137a150fd182672f
Phu Pham <phupham07@yahoo.com>
2020-03-30 13:10:17 -0700

    .

--- code.js
+++ code.js
@@ -1,26 +1,26 @@
 //These are the codes to make default 16x16 boxes.
 var container = document.querySelector('.container');
-for(var y = 0; y < 16; ++y) {
-    for(var x = 0; x < 16; ++x) {
-        container.innerHTML += '<div class ="box"></div>';
-        if(x == 16) break;
-    }
+// for(var y = 0; y < 16; ++y) {
+//     for(var x = 0; x < 16; ++x) {
+//         container.innerHTML += '<div class ="box"></div>';
+//         if(x == 16) break;
+//     }
+// }
+
+//Codes to make the container become black when mouse blackBoxs over for default boxes.
+var blackBox = document.getElementsByClassName('box');
+for(var i = 0; i < blackBox.length; i++) {
+    blackBox[i].addEventListener('mouseover', makeBlack());
 }
 
-//Codes to make the container become black when mouse hovers over for default boxes.
-var hover = document.getElementsByClassName('box');
-for(var i = 0; i < hover.length; i++) {
-    hover[i].addEventListener('mouseover', makeBlack());
-}
-
-var box = container.childNodes;
+// var box = container.childNodes;//need to select specific or may not need this
 var resetButton = document.createElement('button');
 resetButton.classList.add('reset');
 resetButton.textContent = 'Reset';
 resetButton.addEventListener('click', makeWhite());
 container.appendChild(resetButton);
 
-var dimensionButton = document.createElement('button');
+var dimensionButton = document.createElement('button'); 
 dimensionButton.classList.add('dimension');
 dimensionButton.textContent = 'Wanna change dimension of the grid?';
 dimensionButton.addEventListener('click', askUser());
@@ -29,10 +29,10 @@ container.appendChild(dimensionButton);
 
 //below are the functions to be used.
 function makeWhite() {
-    return () => {
-        for (var i = 0; i < box.length; ++i) {
-            box[i].classList.add('white');
-            box[i].classList.remove('black');
+    return (event) => {
+        for (var i = 0; i < event.length; ++i) {
+            event[i].classList.add('white');
+            event[i].classList.remove('black');
         }
     };
 }
@@ -44,24 +44,28 @@ function makeBlack() {
     };
 }
 
-function askUser() {
+function askUser() {//another bug is here; it starts when i refresh the page
     var userInput = prompt('What dimension do you want?');
     alert('You have chose ' +userInput+ 'px.');
     changeDimension(userInput);
 }
 
 function changeDimension(userRequest) {
-    removeChildren(container, box); // bug is here from the function removechildren
-    if(userRequest <= 960) { 
+    // if(userRequest <= 960) { 
         for(var y = 0; y < userRequest; ++y) {
             for(var x = 0; x < userRequest; ++x) {
                 container.innerHTML += '<div class ="userBox"></div>';
                 if(x == userRequest) break;
             }
         }
-    }
-};
+    // }
+}
 
-function removeChildren(node, array) {
-    array.forEach(element => node.classList.removeChild(element)); //bug is here
-};
+// function removeClass() {
+//     for(var y = 0; y < 16; ++y) {
+//         for(var x = 0; x < 16; ++x) {
+//             container.innerHTML += '<div class ="nothing"></div>';
+//             if(x == 16) break;
+//         }
+//     }
+// }
