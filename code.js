2a5fd9f13147e4884bca9524ad753eada572d8a7
Phu Pham <phupham07@yahoo.com>
2020-03-31 20:04:28 -0700

    bugs

--- code.js
+++ code.js
@@ -1,40 +1,28 @@
-//These are the codes to make default 16x16 boxes.
 var container = document.querySelector('.container');
-for(var y = 0; y < 16; ++y) {
-    for(var x = 0; x < 16; ++x) {
-        container.innerHTML += '<div class ="box"></div>';
-        if(x == 16) break;
-    }
-}
+//default grids
+
+startMakingGrids();
 
 //Codes to make the container become black when mouse blackBoxs over for default boxes.
-var blackBox = document.getElementsByClassName('box');
-for(var i = 0; i < blackBox.length; i++) {
-    blackBox[i].addEventListener('mouseover', makeBlack());
+var coloredBox = document.querySelectorAll('.box');
+for(var i = 0; i < coloredBox.length; i++) {
+    coloredBox[i].addEventListener('mouseover', makeBlack());
 }
 
-// var box = container.childNodes;//need to select specific or may not need this
-var resetButton = document.createElement('button');
-resetButton.classList.add('reset');
-resetButton.textContent = 'Reset';
-container.appendChild(resetButton);
-resetButton.addEventListener('click', makeWhite());
+var resetButton = document.querySelector('.reset');
+resetButton.addEventListener('click', resetGrids);
 
-var dimensionButton = document.createElement('button'); 
-dimensionButton.classList.add('dimension');
-dimensionButton.textContent = 'Wanna change dimension of the grid?';
-container.appendChild(dimensionButton);
-dimensionButton.addEventListener('click', askUser());
-
+var dimensionButton = document.querySelector('.dimension'); 
+dimensionButton.addEventListener('click', askUser);
 
 //below are the functions to be used.
-function makeWhite() {
-    return () => {
-        for (var i = 0; i < blackBox.length; ++i) {
-            blackBox[i].classList.add('white');
-            blackBox[i].classList.remove('black');
-        }
-    };
+function resetGrids() {
+    removeChildren(container, coloredBox);
+    startMakingGrids();
+}
+
+function removeChildren(node, array) {
+    array.forEach(element => node.removeChild(element));
 }
 
 function makeBlack() {
@@ -45,27 +33,20 @@ function makeBlack() {
 }
 
 function askUser() {//another bug is here; it starts when i refresh the page
-    var userInput = prompt('What dimension do you want?');
-    alert('You have chose ' +userInput+ 'px.');
-    // changeDimension(userInput);
+    let userInput = prompt('What dimension do you want?');
+    alert('You have chose ' +userInput+ 'px * ' +userInput+ 'px.');
+    startMakingGrids(userInput);
 }
 
-function changeDimension(userRequest) {
-    if(userRequest <= 960) { 
-        for(var y = 0; y < userRequest; ++y) {
-            for(var x = 0; x < userRequest; ++x) {
-                container.innerHTML += '<div class ="userBox"></div>';
-                if(x == userRequest) break;
+function startMakingGrids(grids = 16) {
+    makeGrids(grids);
+}
+
+//default is 16
+function makeGrids(grids) {
+        for(var y = 1; y <= grids; ++y) {
+            for(var x = 1; x <= grids; ++x) {
+                container.innerHTML += '<div class ="box"></div>';
             }
         }
     }
-}
-
-// function removeClass() {
-//     for(var y = 0; y < 16; ++y) {
-//         for(var x = 0; x < 16; ++x) {
-//             container.innerHTML += '<div class ="nothing"></div>';
-//             if(x == 16) break;
-//         }
-//     }
-// }
