a505e98c63d4fa13848e5d55562d8e62d55f5c0a
Phu Pham <phupham07@yahoo.com>
2020-04-23 10:51:03 -0700

    work on makeGrid and changeColor

--- code.js
+++ code.js
@@ -1,52 +1,34 @@
-var container = document.querySelector('.container');
-//default grids
+ 
+var colorInput = document.querySelector("#colorPicker");
+var size = document.querySelector("#sizePicker");
 
-startMakingGrids();
+size.addEventListener("submit", makeGrid);
+function makeGrid(height, width) {
+    for(let i = 1; i <= height; ++height) {
+        for(let i =1; i<= width; ++width) {
 
-//Codes to make the container become black when mouse blackBoxs over for default boxes.
-var coloredBox = document.querySelectorAll('.box');
-for(var i = 0; i < coloredBox.length; i++) {
-    coloredBox[i].addEventListener('mouseover', makeBlack());
-}
-
-var resetButton = document.querySelector('.reset');
-resetButton.addEventListener('click', resetGrids);
-
-var dimensionButton = document.querySelector('.dimension'); 
-dimensionButton.addEventListener('click', askUser);
-
-//below are the functions to be used.
-function resetGrids() {
-    removeChildren(container, coloredBox);
-    startMakingGrids();
-}
-
-function removeChildren(node, array) {
-    array.forEach(element => node.removeChild(element));
-}
-
-function makeBlack() {
-    return (event) => {
-        event.target.classList.add('black');
-        event.target.classList.remove('white');
-    };
-}
-
-function askUser() {//another bug is here; it starts when i refresh the page
-    let userInput = prompt('What dimension do you want?');
-    alert('You have chose ' +userInput+ 'px * ' +userInput+ 'px.');
-    startMakingGrids(userInput);
-}
-
-function startMakingGrids(grids = 16) {
-    makeGrids(grids);
-}
-
-//default is 16
-function makeGrids(grids) {
-        for(var y = 1; y <= grids; ++y) {
-            for(var x = 1; x <= grids; ++x) {
-                container.innerHTML += '<div class ="box"></div>';
-            }
         }
     }
+}
+
+var colorValue = colorInput.value;
+colorInput.addEventListener("input", changeColor);
+function changeColor {
+    
+}
+
+
+/*Note: 
+1/
+-3 things (class sizePicker, id colorPicker (submit), and id pixelCanvas ) 
+need to be stored in variables.
+
+-the value of the color needs to be stored.
+
+2/
+-add event listeners so user can set color values and table sizes.
+
+3/
+-use loops to clear and create table based on user input.
+-each cell has an event listener that sets the background color of the cell to the selected color.
+*/
